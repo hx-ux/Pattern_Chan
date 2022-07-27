45d8a339731335d129f14e160549c68c31709137
hx-ux <68241463+hx-ux@users.noreply.github.com>
2022-07-27 07:05:49 +0000

    Pending changes exported from your codespace

--- src/saveAs/saveAsPNG.js
+++ src/saveAs/saveAsPNG.js
@@ -2,5 +2,5 @@ import { saveSvgAsPng } from "save-svg-as-png";
 
 export default function savePNG(name = "pattern.png", scale = 4, encoderOpt = 1) {
     // https://github.com/exupero/saveSvgAsPng
-    saveSvgAsPng(document.getElementById("SVG"), name, { scale: scale }, { encoderOptions: encoderOpt }, { encoderType: "image/png" });
+    saveSvgAsPng(document.getElementById("SVG"), "result.png", { scale: scale }, { encoderOptions: encoderOpt }, { encoderType: "image/png" });
 }
